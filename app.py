59b1320832dc16333b733889b3a7291ef9a8c646
kusalb <grand.kusal.kb@gmail.com>
2022-09-25 11:55:33 +0545

    test

--- app.py
+++ app.py
@@ -1,14 +1,51 @@
+# import asyncio
+# import signal
+# import os
+
+# import websockets
+
+
+# async def echo(websocket):
+#     async for message in websocket:
+#         await websocket.send(message)
+
+
+# async def main():
+#     # Set the stop condition when receiving SIGTERM.
+#     loop = asyncio.get_running_loop()
+#     stop = loop.create_future()
+#     loop.add_signal_handler(signal.SIGTERM, stop.set_result, None)
+
+#     async with websockets.serve(
+#         echo,
+#         host="",
+#         port=int(os.environ["PORT"]),
+#     ):
+#         await stop
+
+
+# if __name__ == "__main__":
+#     asyncio.run(main())
+
 import asyncio
 import signal
+import json
+import websockets
+from all_vuln_one_var import run_program
 import os
 
-import websockets
-
+async def hello():  # put application's code here
+    await run_program()
+    return json({'about': "Hello world"})
 
 async def echo(websocket):
     async for message in websocket:
-        await websocket.send(message)
+        print(message)
+        message=message.split(" ")
+        if message[0] == "start":
+            final_message = run_program(message [1])
 
+        await websocket.send(json.dump(final_message))
 
 async def main():
     # Set the stop condition when receiving SIGTERM.
@@ -22,6 +59,7 @@ async def main():
         port=int(os.environ["PORT"]),
     ):
         await stop
+
 
 
 if __name__ == "__main__":
